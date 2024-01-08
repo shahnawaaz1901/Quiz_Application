a46f2376908cec12d45c756c8b406eabff5cd9ab
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-01-08 21:03:10 +0530

    Complete the Logic

--- quiz.js
+++ quiz.js
@@ -1,25 +1,34 @@
 const questionObj = {
   category: "Food & Drink",
   id: "qa-1",
-  correctAnswer: "Three",
-  answers: ["Two", "Three ", "Four", "Five"],
+  correctAnswer: "Three ",
+  options: ["Two", "Three ", "Four", "Five"],
   question: "How many pieces of bun are in a Mcdonald's Big Mac?",
 };
-let index = 0;
-
+let scoreNum = 0;
+const { correctAnswer, options } = questionObj;
+//* Fetch Details
 const questionSection = document.querySelector("#question");
 const optionsContainer = document.querySelector("#options");
+const score = document.querySelector("#score");
 
+//* Append Question
 questionSection.textContent = questionObj.question;
-for (let everyOption of questionObj.answers) {
-  const newElement = document.createElement("button");
-  newElement.textContent = everyOption;
-  optionsContainer.appendChild(newElement);
-}
 
-const options = document.querySelectorAll("button");
-for (let every of options) {
-  every.addEventListener("click", function (e) {
-    console.log(e.target.textContent);
+//* Append Options
+options.forEach((opt) => {
+  const newElement = document.createElement("button");
+  newElement.textContent = opt;
+  optionsContainer.appendChild(newElement);
+
+  newElement.addEventListener("click", () => {
+    if (opt == correctAnswer) {
+      scoreNum++;
+    } else {
+      scoreNum -= 0.25;
+    }
+    console.log(scoreNum);
+    optionsContainer.textContent = "";
+    score.textContent = `Score : ${scoreNum}`;
   });
-}
+});
