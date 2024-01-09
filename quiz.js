6ac7791879f20610d50d64b65ac6956a519014a6
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-01-09 22:19:15 +0530

    Start to Implement the Multiple Questions after One

--- quiz.js
+++ quiz.js
@@ -1,14 +1,40 @@
+const questionJSON = [
+  {
+    correctAnswer: ".h",
+    options: [".cpp", ".h", ".hg", ".hg"],
+    question:
+      "Identify the correct extension of the user-defined header file in C++.?",
+  },
+  {
+    correctAnswer: "Friend Constructor",
+    options: [
+      "Friend Constructor",
+      "Default Constructor",
+      "Parameterized Question",
+      "Copy Constructor",
+    ],
+    question: "Identify the incorrect constructor type.?",
+  },
+  {
+    correctAnswer: "bottom-up",
+    options: ["right-left", "top-down", "left-right", "bottom-up"],
+    question: "C++ uses which approach?",
+  },
+  {
+    correctAnswer: "&",
+    options: ["*", "&", "|", "$"],
+    question: "Which of the following is “address of operator”?.",
+  },
+];
+
+let index = 1;
 const questionObj = {
-  category: "Food & Drink",
-  id: "qa-1",
   correctAnswer: "Three",
   options: ["Two", "Three", "Four", "Five"],
   question: "How many pieces of bun are in a Mcdonald's Big Mac?",
 };
 
 let scoreNum = 0;
-const { correctAnswer, options } = questionObj;
-shuffle(options);
 
 //* Fetch Details
 const questionSection = document.querySelector("#question");
@@ -16,25 +42,38 @@ const optionsContainer = document.querySelector("#options");
 const score = document.querySelector("#score");
 
 //* Append Question
-questionSection.textContent = questionObj.question;
 
+shuffleOptions(options);
 //* Append Options
-options.forEach((opt) => {
-  const newElement = document.createElement("button");
-  newElement.textContent = opt;
-  optionsContainer.appendChild(newElement);
+function displayQuestions() {
+  const { correctAnswer, options, question } = questionJSON[index];
+  questionSection.textContent = question;
+  options.forEach((opt) => {
+    const newElement = document.createElement("button");
+    newElement.textContent = opt;
+    optionsContainer.appendChild(newElement);
 
-  newElement.addEventListener("click", () => {
-    if (opt == correctAnswer) {
-      scoreNum++;
-    } else {
-      scoreNum -= 0.25;
-    }
-    optionsContainer.textContent = "";
-    score.textContent = `Score : ${scoreNum}`;
+    newElement.addEventListener("click", () => {
+      if (opt == correctAnswer) {
+        scoreNum++;
+      } else {
+        scoreNum -= 0.25;
+      }
+      // optionsContainer.textContent = "";
+      score.textContent = `Score : ${scoreNum}`;
+    });
   });
-});
+}
 
+document.getElementById("next").addEventListener("click", function () {
+  index++;
+  if (index == questionJSON.length) {
+    score.textContent = `Score : ${scoreNum}`;
+    window.alert("Quiz Completed Check Your Score");
+  }
+  document.getElementById("next").remove();
+  displayQuestions();
+});
 /* 
   Math.random function return Numbers from 0 to 1 like 0.456767, 
   0.678 it can be anything between 0 to 1 
@@ -49,10 +88,11 @@ options.forEach((opt) => {
   the decimal part from that number it can helps us to shuffle elements in the
   array because array not contains the decimal index value
 */
-function shuffle(option) {
-  for (let i = option.length - 1; i > 0; i--) {
+function shuffleOptions(options) {
+  for (let i = options.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * i);
-    [options[i], option[j]] = [option[j], option[i]];
+    [options[i], options[j]] = [options[j], options[i]];
   }
-  return option;
 }
+
+displayQuestions();
