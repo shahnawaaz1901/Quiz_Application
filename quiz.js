4108e79669ec62af9b9c5290f9acb3fc513978d2
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-01-09 19:38:41 +0530

    Added Shuffling of Options using Math class methods

--- quiz.js
+++ quiz.js
@@ -1,12 +1,15 @@
 const questionObj = {
   category: "Food & Drink",
   id: "qa-1",
-  correctAnswer: "Three ",
-  options: ["Two", "Three ", "Four", "Five"],
+  correctAnswer: "Three",
+  options: ["Two", "Three", "Four", "Five"],
   question: "How many pieces of bun are in a Mcdonald's Big Mac?",
 };
+
 let scoreNum = 0;
 const { correctAnswer, options } = questionObj;
+shuffle(options);
+
 //* Fetch Details
 const questionSection = document.querySelector("#question");
 const optionsContainer = document.querySelector("#options");
@@ -27,8 +30,29 @@ options.forEach((opt) => {
     } else {
       scoreNum -= 0.25;
     }
-    console.log(scoreNum);
     optionsContainer.textContent = "";
     score.textContent = `Score : ${scoreNum}`;
   });
 });
+
+/* 
+  Math.random function return Numbers from 0 to 1 like 0.456767, 
+  0.678 it can be anything between 0 to 1 
+*/
+
+/* 
+  If we want number from 0 to any number then we need to multiply Math.random()
+  with the number which we want between 0 to them
+*/
+/* 
+  Math.floor is the function which converts any number into the integer and removes
+  the decimal part from that number it can helps us to shuffle elements in the
+  array because array not contains the decimal index value
+*/
+function shuffle(option) {
+  for (let i = option.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * i);
+    [options[i], option[j]] = [option[j], option[i]];
+  }
+  return option;
+}
